3407157bd637569448e0f210804e3404061c4ef7
JeysonFlores <jeyson.flores@protonmail.com>
2022-05-15 17:05:09 -0600

    ErrorModal done

--- app/src/main/java/site/xreader/xreaderandroid/LoginFragment.java
+++ app/src/main/java/site/xreader/xreaderandroid/LoginFragment.java
@@ -1,5 +1,6 @@
 package site.xreader.xreaderandroid;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,11 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
+import site.xreader.xreaderandroid.utils.BackendProxy;
+
 public class LoginFragment extends Fragment {
     private EditText usernameTxt;
     private EditText passwordTxt;
     private Button loginBtn;
     private Button signupBtn;
+
+    private BackendProxy backend;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,21 +34,65 @@ public class LoginFragment extends Fragment {
         loginBtn = (Button) main_view.findViewById(R.id.loginBtn);
         signupBtn = (Button) main_view.findViewById(R.id.signinBtn);
 
+        backend = new BackendProxy();
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Login", Toast.LENGTH_SHORT).show();
+                String username = usernameTxt.getText().toString();
+                String password = passwordTxt.getText().toString();
+
+                if(validateText(username, password)) {
+                    backend.login(username, password, () -> {
+                        Toast.makeText(getContext(), "Token: " + backend.getToken(), Toast.LENGTH_SHORT).show();
+                    }, (error) -> {
+                        String errMsg;
+
+                        switch(error) {
+                            case "Wrong credentials":
+                                errMsg = "Usuario o contraseña incorrectos. Verifique la información ant" +
+                                        "antes de continuar.";
+                                break;
+                            default:
+                                errMsg = "Ha habido un error al hacer el inicio de sesión.";
+                        }
+
+                        showErrorDialog(errMsg);
+                    });
+                } else {
+                    showErrorDialog("Ingrese datos válidos por favor.");
+                }
             }
         });
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Signup", Toast.LENGTH_SHORT).show();
+                Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.error_modal);
+
+                EditText msgErrorTxt = (EditText) dialog.findViewById(R.id.messageErrorTxt);
+                msgErrorTxt.setText("XDDDDDDDDDDDDDD");
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.modal_rounded);
+                dialog.show();
             }
         });
 
         return main_view;
+    }
+
+    private boolean validateText(String username, String password) {
+        return !(username.replace(" ", "") == "" || password.replace(" ", "") == "");
+    }
+
+    private void showErrorDialog(String msg) {
+        Dialog dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.error_modal);
+
+        EditText msgErrorTxt = (EditText) dialog.findViewById(R.id.messageErrorTxt);
+        msgErrorTxt.setText(msg);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.modal_rounded);
+        dialog.show();
     }
 
 
