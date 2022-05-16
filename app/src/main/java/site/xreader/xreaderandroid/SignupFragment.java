07d6322b3e71b80b6863e810bcb6a4c78dc069e7
JeysonFlores <jeyson.flores@protonmail.com>
2022-05-15 23:12:07 -0600

    Signup Design Done

--- app/src/main/java/site/xreader/xreaderandroid/SignupFragment.java
+++ app/src/main/java/site/xreader/xreaderandroid/SignupFragment.java
@@ -3,17 +3,32 @@ package site.xreader.xreaderandroid;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SignupFragment extends Fragment {
+
+    private Button loginBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        View main_view = inflater.inflate(R.layout.fragment_signup,container,false);
+
+        loginBtn = (Button) main_view.findViewById(R.id.toLoginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.scenario, new LoginFragment()).commit();
+            }
+        });
+
+        return main_view;
     }
 }
