33d4175f4f823baf57a4ab21303817ad5c446242
JeysonFlores <jeyson.flores@protonmail.com>
2022-05-16 19:24:05 -0600

    Splash Screen almost done

---
+++ app/src/main/java/site/xreader/xreaderandroid/SplashFragment.java
@@ -0,0 +1,35 @@
+package site.xreader.xreaderandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View main_view = inflater.inflate(R.layout.fragment_splash,container,false);
+        ImageView logoImg = (ImageView) main_view.findViewById(R.id.splashLogoImg);
+
+        logoImg.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.bounce));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.scenario, new LoginFragment()).commit();
+            }
+        }, 3000);
+
+        return main_view;
+    }
+}
